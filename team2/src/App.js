fdd42434e4558bb45759edde6efa4d1afe5aebb0
lorebell <37243576+lorebell@users.noreply.github.com>
2022-07-22 01:23:21 -0300

    NavBar li

--- team2/src/App.js
+++ team2/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from './components/home/HomeScreen';
 import NavBar from './components/ui/NavBar';
--- team2/src/components/ui/NavBar.js
+++ team2/src/components/ui/NavBar.js
@@ -4,10 +4,22 @@ import logo from '../../logo.svg';
 
 export default function NavBar() {
   return (
- 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+
+    <header className="App-header">
+      <div className='container-fluid bg-dark naav'>
+        <div className='row'>
+          <div className='col'></div>
+          <div className='col'>
+            <ul>
+              <li><a href="/">Nosotros</a></li>
+              <li><a href="/">Nuestros Clientes</a></li>
+              <li><a href="/">Contactanos</a></li>
+              <li><a href="/">Trabajá con nosotros</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
 
   )
 }
