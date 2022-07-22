5b83b5d8de028459bd5ea88a9b339aae6b5a53f0
lorebell <37243576+lorebell@users.noreply.github.com>
2022-07-21 22:29:27 -0300

    commit inicial

---
+++ team2/src/components/ui/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../assets/css/navbar.css'
+import logo from '../../logo.svg';
+
+export default function NavBar() {
+  return (
+ 
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+
+  )
+}
